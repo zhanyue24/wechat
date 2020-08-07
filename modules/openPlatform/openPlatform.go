452faa4e146693efc3e2e31c703c29432b382cb8
zhany <115300581@qq.com>
2020-08-07 16:49:18 +0800

    fix:

---
+++ modules/openPlatform/openPlatform.go
@@ -0,0 +1,116 @@
+package openPlatform
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/ddliu/go-httpclient"
+	"github.com/sirupsen/logrus"
+	"github.com/zhanyue24/wechat/config"
+)
+
+type OpenPlatform struct {
+	Config config.OpenPlatformCfg
+}
+
+func New(cfg config.OpenPlatformCfg) *OpenPlatform {
+
+	logrus.WithField("data", cfg).Info("load config")
+
+	return &OpenPlatform{
+		Config: cfg,
+	}
+}
+
+type GetUserInfoReturn struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    string `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	OpenId       string `json:"openid"`
+	Scope        string `json:"scope"`
+	Unionid      string `json:"unionid"`
+}
+
+// https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID
+func (m *OpenPlatform) GetUserInfo(authInfo GetAccessTokenReturn) (ret GetUserInfoReturn, err error) {
+	params := map[string]string{
+		"access_token": authInfo.AccessToken,
+		"openid":       authInfo.OpenId,
+		"lang":         "zh-CN",
+	}
+
+	result, err := m.http("userinfo", params)
+	if err != nil {
+		return
+	}
+
+	json.Unmarshal(result, &ret)
+	logrus.Info(result)
+
+	return
+}
+
+type GetAccessTokenReturn struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    string `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	OpenId       string `json:"openid"`
+	Scope        string `json:"scope"`
+	Unionid      string `json:"unionid"`
+}
+
+func (m *OpenPlatform) GetAccessToken(code string) (ret GetAccessTokenReturn, err error) {
+	grantType := "authorization_code"
+
+	params := map[string]string{
+		"grant_type": grantType,
+		"appid":      m.Config.AppId,
+		"secret":     m.Config.AppSecret,
+		"code":       code,
+	}
+
+	result, err := m.http("oauth2/access_token", params)
+	if err != nil {
+		return
+	}
+
+	json.Unmarshal(result, &ret)
+
+	return
+}
+
+type HttpErr struct {
+	ErrCode int64  `json:"errcode"`
+	ErrMsg  string `json:"errmsg"`
+}
+
+type HttpReturn struct {
+	HttpErr
+}
+
+func (m *OpenPlatform) http(path string, params map[string]string) (ret []byte, err error) {
+
+	baseUrl := "https://api.weixin.qq.com/sns/"
+	url := fmt.Sprintf("%s%s", baseUrl, path)
+
+	res, err := httpclient.Get(url, params)
+	defer res.Body.Close()
+
+	ret, err = res.ReadAll()
+
+	var jsonData HttpErr
+
+	json.Unmarshal(ret, &jsonData)
+
+	if jsonData.ErrCode > 0 {
+		err = errors.New(fmt.Sprintf("[%d] %s", jsonData.ErrCode, jsonData.ErrMsg))
+		logrus.WithError(err).WithFields(logrus.Fields{
+			"url":    url,
+			"params": params,
+		}).Warning("return error")
+		return
+	}
+
+	return
+}
